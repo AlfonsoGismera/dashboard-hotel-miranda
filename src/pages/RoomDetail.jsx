d8f29452897c09899f7a809d9c6db4b5f0391316
Alfonso <alfonsofcogismeraperea@gmail.com>
2025-05-18 15:32:52 +0200

    Pruebas

--- src/pages/RoomDetail.jsx
+++ src/pages/RoomDetail.jsx
@@ -1,2 +1,2 @@
 import { useParams } from 'react-router-dom';
-export default function RoomDetail() { const { id } = useParams(); return <h1>Room Prueba {id}</h1>; }
+export default function RoomDetail() { const { id } = useParams(); return <h1>Room Prueba2 {id}</h1>; }
